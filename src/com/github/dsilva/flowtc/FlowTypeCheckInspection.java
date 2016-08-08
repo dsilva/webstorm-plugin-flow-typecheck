c4c872bf88d197a40aaef2d22c8a8afdd4bc91cc
Daniel Silva <danielsi@amazon.com>
2016-08-08 19:02:19 +0100

    switch to external annotator

--- src/com/github/dsilva/flowtc/FlowTypeCheckInspection.java
+++ src/com/github/dsilva/flowtc/FlowTypeCheckInspection.java
@@ -1,22 +1,13 @@
 package com.github.dsilva.flowtc;
 
-// already bundled with IntelliJ IDEA and WebStorm
-import com.google.gson.Gson;
 
-import com.intellij.codeInspection.*;
+import com.intellij.codeInspection.InspectionManager;
+import com.intellij.codeInspection.LocalInspectionTool;
+import com.intellij.codeInspection.ProblemDescriptor;
+import com.intellij.codeInspection.ProblemHighlightType;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.fileEditor.FileDocumentManager;
-import com.intellij.openapi.project.Project;
-import com.intellij.openapi.util.TextRange;
-import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 
 /**
@@ -24,216 +15,18 @@ import java.util.Random;
  */
 public class FlowTypeCheckInspection extends LocalInspectionTool {
     private static final Logger log = Logger.getInstance(FlowTypeCheckInspection.class);
-    private static final ProblemDescriptor[] noProblems = new ProblemDescriptor[] {};
-    private static final Gson gson = new Gson();
-
-    private static class FlowMessagePart {
-        String descr;
-        //public String level; // "error"
-        String path;
-        int line;
-        int endline;
-        int start;
-        int end;
-    }
-
-    private static class FlowError {
-        ArrayList<FlowMessagePart> message;
-        //public String kind; // "infer"
-    }
-
-    private static class FlowResponse {
-        boolean passed;
-        ArrayList<FlowError> errors;
-        //public String version;
-    }
-
 
     @NotNull
     public ProblemDescriptor[] checkFile(@NotNull final PsiFile file, @NotNull final InspectionManager manager, final boolean isOnTheFly) {
-        log.debug("Flow checkFile", file);
-
-        final VirtualFile vfile = file.getVirtualFile();
-        if (vfile == null) {
-            log.error("missing vfile for " + file);
-            return noProblems;
-        }
-
-        final VirtualFile vparent = vfile.getParent();
-        if (vparent == null) {
-            log.error("missing vparent for " + file);
-            return noProblems;
-        }
-
-        final String path = vfile.getCanonicalPath();
-        if (path == null) {
-            log.error("missing canonical path for " + file);
-            return noProblems;
-        }
-
-        final String dir = vparent.getCanonicalPath();
-        if (dir == null) {
-            log.error("missing canonical dir for " + file);
-            return noProblems;
-        }
-
-        vfile.getParent().getCanonicalPath();
-
-        final String text = file.getText();
-        if (text == null) {
-            log.error("missing text for " + file);
-            return noProblems;
-        }
-
-        final String flowOutput = flowCheck(dir, text);
-        log.debug("flow output", flowOutput);
-
-        if (flowOutput.isEmpty()) {
-            log.error("flow output was empty");
-            return noProblems;
-        }
-
-        final FlowResponse response = gson.fromJson(flowOutput, FlowResponse.class);
-
-        if (response == null) {
-            log.error("could not parse flow output " + flowOutput);
-            return noProblems;
-        }
-
-        if (response.passed) {
-            log.info("flow passed");
-            return noProblems;
-        }
-        if (response.errors == null) {
-            log.error("flow failed, but shows no errors");
-            return noProblems;
-        }
-
-        final Document document = FileDocumentManager.getInstance().getDocument(vfile);
-
-        if (document == null) {
-            log.error("missing document");
-            return noProblems;
-        }
-
-        final List<ProblemDescriptor> descriptors = new ArrayList<>();
-
-        for (final FlowError error: response.errors) {
-            final ArrayList<FlowMessagePart> messageParts = error.message;
-            if (messageParts == null || messageParts.size() == 0) {
-                log.error("flow missing message in error " + error);
-                continue;
-            }
-
-            final FlowMessagePart firstPart = messageParts.get(0);
-            if (!path.equals(firstPart.path) && !"-".equals(firstPart.path)) {
-                log.info("skip error because first message part path " + firstPart.path + " does not match file path " + path);
-                continue;
-            }
-
-            final StringBuilder errorMessageBuilder = new StringBuilder(firstPart.descr);
-
-            for (int i = 1; i < messageParts.size(); i++) {
-                final FlowMessagePart messagePart = messageParts.get(i);
-                if (messagePart.path == null || messagePart.path.isEmpty()) {
-                    errorMessageBuilder.append(". ");
-                } else {
-                    errorMessageBuilder.append(" ");
-                }
-                errorMessageBuilder.append(messagePart.descr);
-            }
-
-            final String errorMessage = errorMessageBuilder.toString();
-            log.info("Flow found error: " + errorMessage);
-
-            for (final FlowMessagePart part: error.message) {
-                if (part.path.isEmpty()) {
-                    // skip part of error message that has no file/line reference
-                    continue;
-                }
-                if (!path.equals(part.path) && !"-".equals(part.path)) {
-                    // skip part of error message that refers to content in another file
-                    continue;
-                }
-
-                final int lineStartOffset = document.getLineStartOffset(part.line - 1);
-                final int lineEndOffset = document.getLineStartOffset(part.endline - 1);
-
-                final ProblemDescriptor problemDescriptor = manager.createProblemDescriptor(
+        log.info("Flow inspection running");
+        return TypeCheck.errors(file).stream()
+                .map(error -> manager.createProblemDescriptor(
                         file,
-                        TextRange.create(lineStartOffset + part.start - 1, lineEndOffset + part.end),
-                        errorMessage,
+                        error.range(),
+                        error.message(),
                         ProblemHighlightType.ERROR,
-                        isOnTheFly);
-
-                log.info("Flow error for file " + file + " at " + part.line + ":" + part.start + " to " + part.endline + ":" + part.end + " range " + TextRange.create(lineStartOffset + part.start - 1, lineEndOffset + part.end));
-                descriptors.add(problemDescriptor);
-            }
-        }
-        if (descriptors.isEmpty()) {
-            return noProblems;
-        } else {
-            log.info("Flow inspector found problem descriptors " + descriptors);
-            return descriptors.toArray(new ProblemDescriptor[descriptors.size()]);
-        }
+                        isOnTheFly))
+                .toArray(ProblemDescriptor[]::new);
     }
 
-    private static final Random random = new Random();
-
-    @NotNull
-    private static String flowCheck(@NotNull final String dir, @NotNull String text) {
-        final File workingDir = new File(dir);
-        log.debug("flowCheck working directory", workingDir);
-
-        final String[] cmd = new String[] {Settings.readPath(),
-                "check-contents",
-                "--show-all-errors", "--json"};
-
-        try {
-            final Process process = Runtime.getRuntime().exec(
-                    cmd, null, workingDir);
-
-            final StringBuilder outString = new StringBuilder();
-            final Thread outThread = readStream(outString, process.getInputStream());
-
-            final StringBuilder errString = new StringBuilder();
-            final Thread errorThread = readStream(errString, process.getErrorStream());
-
-            outThread.start();
-            errorThread.start();
-
-            // Send the file text through stdin instead of letting Flow file it in the filesystem
-            // because IntelliJ might not have written the latest changes yet.
-            final OutputStream os = process.getOutputStream();
-            os.write(text.getBytes());
-            os.close();
-
-            outThread.join();
-            errorThread.join();
-
-            final int exitCode = process.waitFor();
-            log.debug("flow exited with code ", exitCode);
-
-            return outString.toString();
-        } catch (InterruptedException | IOException e) {
-            e.printStackTrace();
-        }
-        return "";
-    }
-
-    private static Thread readStream(@NotNull final StringBuilder outString, @NotNull final InputStream inputStream) {
-        return new Thread(() -> {
-            try {
-                String line;
-                try (BufferedReader outStream = new BufferedReader(
-                        new InputStreamReader(inputStream))) {
-                    while ((line = outStream.readLine()) != null) {
-                        outString.append(line).append("\n");
-                    }
-                }
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        });
-    }
 }
